1851bae6965a38c79fe31d47b65b2bbb57f5cd77
Siddharth <sidn@mit.edu>
2016-11-26 10:39:35 -0500

    refactoring

--- Tools/interface/Functions.h
+++ Tools/interface/Functions.h
@@ -1,55 +1,6 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef FUNCTIONS2_H
+#define FUNCTIONS2_H
 
-#include "TLorentzVector.h" //ugh
-#include "TMath.h"
-
-#define PI 3.141592654
-
-/** \file Functions.h
- * \brief Some numerical functions
- */
-
-//////////////////////////////////////////////////////////////////////////////////
-
-/**
- * \brief Invariant mass of a pair of particles
- */
-inline double Mxx(double pt1, double eta1, double phi1, double m1, double pt2, double eta2, double phi2, double m2) {
-  TLorentzVector v1,v2;
-  v1.SetPtEtaPhiM(pt1,eta1,phi1,m1);
-  v2.SetPtEtaPhiM(pt2,eta2,phi2,m2);
-  return (v1+v2).M();
-}
-
-/**
- * \brief Signed delta-phi
- */
-inline double SignedDeltaPhi(double phi1, double phi2) {
-  double dPhi = phi1-phi2;
-  if (dPhi<-PI)
-    dPhi = 2*PI+dPhi;
-  else if (dPhi>PI)
-    dPhi = -2*PI+dPhi;
-  return dPhi;
-}
-
-/**
- * \brief Calculates the delta-R-squared metric
- */
-inline double DeltaR2(double eta1, double phi1, double eta2, double phi2) {
-  float dEta2 = (eta1-eta2); dEta2 *= dEta2;
-  float dPhi = SignedDeltaPhi(phi1,phi2);
-  return dEta2 + dPhi*dPhi;
-}
-
-/**
- * \brief Exponential times erf, aka CMSShape
- */
-inline double ExpErf(double x, double a, double b, double c) {
-  double exp_ = TMath::Exp(c*x);
-  double erf_ = TMath::Erf((x-a)/b);
-  return exp_*(1+erf_)/2;
-}
+#include "PandaUtilities/Common/interface/NumericalFunctions.h"
 
 #endif
