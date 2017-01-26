720edd00070c5c2f07e1a8d0f73c329ea33ebb20
Siddharth <sidn@mit.edu>
2017-01-26 07:59:22 -0500

    adding shell logging tools

---
+++ Tools/bin/PWarning.cc
@@ -0,0 +1,25 @@
+#include "../interface/Common.h"
+#include "string"
+
+int main( int argc, char **argv ) {
+	std::string name = "shell";
+	std::string report = "";
+	bool is_name=false;
+	for (int iA=1; iA!=argc; ++iA) {
+		std::string this_arg = argv[iA];
+		if (this_arg=="-n") {
+			is_name = true;
+			continue;
+		}
+		if (is_name) {
+			name = this_arg;
+			is_name = false;
+			continue;
+		}
+		if (iA>1)
+			report += " ";
+		report += this_arg;
+	}
+
+	PWarning(name.c_str(),report.c_str());
+}
