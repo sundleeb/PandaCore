1d21bf28b166c3287e2564171c00ca6638e37c1b
Siddharth <sidn@mit.edu>
2017-01-26 07:48:14 -0500

    cleanup

--- Tools/bin/checkJobs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then echo "provide name please"; exit; fi; for jid in $( condor_q $USER -global | grep -o "^[0-9]*\." ); do if [ "$(condor_q $jid -global -long | grep UserLog | grep $1)" != "" ]; then condor_q $jid | tail -n 1; fi;  done
--- Tools/bin/checkNJobs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then echo "provide name please"; exit; fi; condor_q $USER -global -long | grep UserLog | grep -c $1
