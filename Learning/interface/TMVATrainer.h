1f2eeaac9df879871cf21ead1daaf1e7ef282aa6
Benedikt Maier <benedikt.maier@cern.ch>
2017-11-16 21:22:40 -0500

    TMVA in ROOT >=6.08

--- Learning/interface/TMVATrainer.h
+++ Learning/interface/TMVATrainer.h
@@ -62,7 +62,7 @@ public:
   TString sigweight="", bgweight=""; /**< weights to be applied to signal and background events */
 private:
   TMVA::Factory *factory=0; /**< the real workhorse, used to book and train methods */
-  TMVA::Dataloader *dataloader=0; /**< the real workhorse #2 */
+  TMVA::DataLoader *dataloader=0; /**< the real workhorse #2 */
   TFile *sigfile=0, *bgfile=0; /**<  pointers to the input files */
   TTree *sigtree=0, *bgtree=0; /**< input trees */
   TFile *outfile; /**< pointer to output files, owned by this object */
